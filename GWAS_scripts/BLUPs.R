944b51f5fef52d5768502ef8a28f9ce6c351d343
selizpowers <33422000+selizpowers@users.noreply.github.com>
2021-04-14 14:51:50 -0400

    Update BLUPs.R

--- GWAS_scripts/BLUPs.R
+++ GWAS_scripts/BLUPs.R
@@ -1,11 +1,12 @@
 library(lme4)
-df <- read.csv("IP6blups.csv")
+df <- read.csv("blups.csv")
 head(df)
+#add for all variables to incorporate in model
 df$REP <- as.factor(df$REP)
 df$TAXA <- as.factor(df$TAXA)
 df$DATE1 <- as.factor(df$DATE)
-first_trait_column = 4
-trait_columns = 0
+first_trait_column = 4 #where first phenotype appears
+trait_columns = 0 #number of trait columns - 1
 for (i in first_trait_column :first_trait_column + trait_columns){
 its_only_a_model <- lme4::lmer(paste0(names(df[i])[1], " ~(1|TAXA) + (1|DATE)"), data = df)
 random_effects <- lme4::ranef(its_only_a_model)
